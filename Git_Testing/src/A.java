a6712af52f0f9de696019b71d422e74f652c1e9c
SmitaP123 <107536968+SmitaP123@users.noreply.github.com>
2022-06-15 13:28:44 +0530

    Update A.java

--- Git_Testing/src/A.java
+++ Git_Testing/src/A.java
@@ -5,17 +5,9 @@ public class A
 	{
 		System.out.println("dfgiudcbjkf");
 		System.out.println("dfgiudcbjkf");
-		
+		System.out.println("dfgiudcbjkf");
 		
 	}
-	public void m1()
-		{
-			System.out.println("dfgiudcbjkf");
-			
-		}
-public void m2()
-		{
-			System.out.println("dfgiudcbjkf");
-			
-		}
+	
+	
 }
